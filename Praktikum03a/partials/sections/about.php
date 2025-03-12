57f247e54f30e2ed3a8c941611659c229140f30e
Rex1982 <yudhistirataufik1928@gmail.com>
2025-03-12 17:05:10 +0700

    Praktikum03

---
+++ Praktikum03a/partials/sections/about.php
@@ -0,0 +1,19 @@
+<section class="page-section bg-primary text-white mb-0" id="about">
+            <div class="container">
+                <!-- About Section Heading-->
+                <h2 class="page-section-heading text-center text-uppercase text-white">About</h2>
+                <!-- Icon Divider-->
+                <div class="divider-custom divider-light">
+                    <div class="divider-custom-line"></div>
+                    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                    <div class="divider-custom-line"></div>
+                </div>
+                <!-- About Section Content-->
+                <div class="row">
+                    <div class="col-lg-4 ms-auto"><p class="lead">I’m a data analyst and web developer who loves uncovering insights from data and designing websites that are both visually appealing and functional. I’m passionate about turning raw data into meaningful stories and building web applications that are easy to use and accessible to everyone. As a self-learner, I’m always eager to take on new challenges and stay updated with the latest trends in data and web technologies.
+</p></div>
+                    <div class="col-lg-4 me-auto"><p class="lead">I have experience working with Python and SQL for data analysis, as well as visualizing insights using tools like Tableau and Power BI. On the web development side, I’m skilled in HTML, CSS, and JavaScript, and familiar with modern frameworks like React and Bootstrap. I’m always looking for ways to improve my skills and keep up with the ever-evolving world of data and web development.</p></div>
+                </div>
+                <!-- About Section Button-->
+                
+        </section>
---
+++ Praktikum03b/nav.php
@@ -0,0 +1,64 @@
+            <nav class="navbar navbar-expand-lg navbar-light bg-white py-3">
+                <div class="container px-5">
+                    <a class="navbar-brand" href="index.php"><span class="fw-bolder text-primary">My Profile</span></a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav ms-auto mb-2 mb-lg-0 small fw-bolder">
+                            <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+                            <li class="nav-item"><a class="nav-link" href="resume.php">Resume</a></li>
+                            <li class="nav-item"><a class="nav-link" href="projects.php">Projects</a></li>
+                            <li class="nav-item"><a class="nav-link" href="contact.php">Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <!-- Header-->
+            <header class="py-5">
+                <div class="container px-5 pb-5">
+                    <div class="row gx-5 align-items-center">
+                        <div class="col-xxl-5">
+                            <!-- Header text content-->
+                            <div class="text-center text-xxl-start">
+                                <div class="badge bg-gradient-primary-to-secondary text-white mb-4"><div class="text-uppercase">Data analyst &middot; Web Development &middot; Web Design</div></div>
+                                <div class="fs-3 fw-light text-muted">Web Development - Yudhistira Taufik</div>
+                                <h1 class="display-3 fw-bolder mb-5"><span class="text-gradient d-inline"></span></h1>
+                                <div class="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xxl-start mb-3">
+                                    <a class="btn btn-primary btn-lg px-5 py-3 me-sm-3 fs-6 fw-bolder" href="resume.php">Resume</a>
+                                    <a class="btn btn-outline-dark btn-lg px-5 py-3 fs-6 fw-bolder" href="projects.php">Projects</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xxl-7">
+                            <!-- Header profile picture-->
+                            <div class="d-flex justify-content-center mt-5 mt-xxl-0">
+                                <div class="profile bg-gradient-primary-to-secondary">
+                                    <!-- TIP: For best results, use a photo with a transparent background like the demo example below-->
+                                    <!-- Watch a tutorial on how to do this on YouTube (link)-->
+                                    <img class="profile-img" src="assets/profil.png" width="300" height="300" alt="..." />
+                                    
+            </header>
+            <!-- About Section-->
+            <section class="bg-light py-5">
+                <div class="container px-5">
+                    <div class="row gx-5 justify-content-center">
+                        <div class="col-xxl-8">
+                            <div class="text-center my-5">
+                                <h2 class="display-5 fw-bolder"><span class="text-gradient d-inline">About Me</span></h2>
+                                <p class="lead fw-light mb-4">
+
+                                </p>"As a web developer, I’m passionate about building responsive, user-friendly websites that not only look great but also perform seamlessly. With expertise in HTML, CSS, JavaScript, and modern frameworks, I strive to create innovative digital solutions while continuously learning and adapting to new technologies.
+                                <p class="text-muted">
+
+                                </p>
+                                <div class="d-flex justify-content-center fs-2 gap-4">
+                                    <a class="text-gradient" href="#!"><i class="bi bi-twitter"></i></a>
+                                    <a class="text-gradient" href="https://www.linkedin.com/in/yudhistira-taufik/"><i class="bi bi-linkedin"></i></a>
+                                    <a class="text-gradient" href="https://github.com/Yudhis1928"><i class="bi bi-github"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </main>
+        <!-- Footer-->
