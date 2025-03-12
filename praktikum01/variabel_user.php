195114fa8ab94d23314a55e4fb0d81eff109ffb9
Rex1982 <yudhistirataufik1928@gmail.com>
2025-03-12 17:33:10 +0700

    Praktikum01

---
+++ praktikum01/variabel_user.php
@@ -0,0 +1,11 @@
+<?php
+// medefinisikan variabel
+$nama = 'Huda';
+$umur = 23;
+$berat = 55;
+
+echo 'Nama saya '. $nama;
+echo '<br/>Umur: '. $umur . ' Tahun';
+echo '<br/>Berat: '. $berat . ' Kg';
+
+?>
