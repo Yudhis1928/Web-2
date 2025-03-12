10c8f412b016468c8020beb0448721ebda0d09c6
Rex1982 <yudhistirataufik1928@gmail.com>
2025-03-12 17:20:58 +0700

    Praktikum02

---
+++ Praktikum03a/partials/footer.php
@@ -0,0 +1,33 @@
+ <footer class="footer text-center">
+            <div class="container">
+                <div class="row">
+                    <!-- Footer Location-->
+                    <div class="col-lg-4 mb-5 mb-lg-0">
+                        <h4 class="text-uppercase mb-4">Location</h4>
+                        <p class="lead mb-0">
+                            Jl.Haji Miing, Kp.Sindangpala No. 18
+                            <br />
+                            Bogor, Jawa Barat
+                        </p>
+                    </div>
+                    <!-- Footer Social Icons-->
+                    <div class="col-lg-4 mb-5 mb-lg-0">
+                        <h4 class="text-uppercase mb-4">Around the Web</h4>
+                        <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-facebook-f"></i></a>
+                        <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-twitter"></i></a>
+                        <a class="btn btn-outline-light btn-social mx-1" href="https://www.linkedin.com/in/yudhistira-taufik/"><i class="fab fa-fw fa-linkedin-in"></i></a>
+                        <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-dribbble"></i></a>
+                    </div>
+                    <!-- Footer About Text-->
+                    <div class="col-lg-4">
+                        <h4 class="text-uppercase mb-4">About Me</h4>
+                        <p class="lead mb-0">
+                            Making Sense Of Data And Crafting Web Experiences That Truly Matter.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+         <div class="copyright py-4 text-center text-white">
+            <div class="container"><small>Copyright &copy; <?= date("Y")?>Yudhistira Taufik</small></div>
+        </div>
